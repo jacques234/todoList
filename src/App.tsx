dcbddd2a8392031a96538755724139d423d589f8
Diego Perez <perezmolinadiego030@gmail.com>
2025-03-18 22:19:47 -0600

    feat: implementacion de toast al eliminar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
-import { Input, ButtonAdd,Task } from './components'
+import { Input, ButtonAdd, Task } from './components'
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   const [task, setTask] = useState<string>('')
@@ -8,17 +9,19 @@ function App() {
   const inputRef = useRef<HTMLInputElement>(null);
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const newTask : string = task.trim()
-    if(newTask.length === 0) return
+    const newTask: string = task.trim()
+    if (newTask.length === 0) return
     setTareas([...tareas, newTask]);
     setTask('');
     if (inputRef.current) {
       inputRef.current.focus();
     }
   }
-  function handleDeleteTask(index: number){
-    const newTasks :string[] = tareas.filter((_,i) => i !== index)
+  const notify = () => toast("Tarea eliminada");
+  function handleDeleteTask(index: number) {
+    const newTasks: string[] = tareas.filter((_, i) => i !== index)
     setTareas(newTasks)
+    notify();
   }
 
   return (
@@ -26,21 +29,22 @@ function App() {
       <main>
         <h1>Lista de tareas</h1>
         <section className='todo_section'>
-          <form className="todo_section__add" onSubmit={ handleSubmit }>
-            <Input id='input_task' name='newTask' value={task} ref={inputRef} parentMethod={(e) => setTask(e.target.value)}/>
+          <form className="todo_section__add" onSubmit={handleSubmit}>
+            <Input id='input_task' name='newTask' value={task} ref={inputRef} parentMethod={(e) => setTask(e.target.value)} />
             <ButtonAdd id='btnAddTask' />
           </form>
           <section className='todo_section__task'>
             {
-              tareas.length === 0 ? <p>No hay tareas. ¡Añade una!</p> : 
-              (
-                tareas.map((task,index) => (
-                  <Task key={index} task={task} parentMethod={() => handleDeleteTask(index)}></Task>
-                  // <li key={index}>{task}</li>
-                ))
-              )
+              tareas.length === 0 ? <p>No hay tareas. ¡Añade una!</p> :
+                (
+                  tareas.map((task, index) => (
+                    <Task key={index} task={task} parentMethod={() => handleDeleteTask(index)}></Task>
+                    // <li key={index}>{task}</li>
+                    
+                  ))
+                )
             }
-            
+            <ToastContainer />
           </section>
         </section>
       </main>
